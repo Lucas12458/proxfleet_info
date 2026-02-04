3f744017d1cc2beec42fee1cb5a59beb8794ea10
David <David.Nabel-Moris@etu.univ-smb.fr>
2026-02-04 16:31:16 +0100

    t

--- api/routers/manager.py
+++ api/routers/manager.py
@@ -153,32 +153,3 @@ async def get_next_vmid(proxmox_manager:ProxmoxManager = Depends(get_proxmox_man
     return proxmox_manager.get_next_vmid()
 
 
-
-@router.get("/sheet/assignments")
-async def get_vm_assignments():
-    url = (f"https://docs.google.com/spreadsheets/d/1vht7HaV6jwAwHuT93ZJXDnu7BOzEcsWD/gviz/tq?tqx=out:csv&sheet=Feuille1")
-
-    async with httpx.AsyncClient(timeout=20) as client:
-        resp = await client.get(url)
-
-    if resp.status_code != 200:
-        raise HTTPException(status_code=502, detail="Failed to fetch Google Sheet")
-
-    reader = csv.DictReader(io.StringIO(resp.text))
-
-    data = []
-    for row in reader:
-        if any(v and v.strip() for v in row.values()):
-            data.append({
-                "promotion": row["Promotion"],
-                "nom": row["Nom"],
-                "prenom": row["Prenom"],
-                "uid": row["uid"],
-                "server_id": int(row["Serveur"]),
-                "server_name": row["Nom-serveur"],
-            })
-
-    return {
-        "count": len(data),
-        "data": data
-    }
