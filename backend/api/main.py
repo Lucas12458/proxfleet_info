a2870a82e9f707ba42871a329872828a73a7e0a2
Lucas12458 <112438781+Lucas12458@users.noreply.github.com>
2026-02-27 00:03:44 +0100

    feat: Add github action

--- backend/api/main.py
+++ backend/api/main.py
@@ -6,16 +6,17 @@ import os
 
 description = """"""
 
-IS_PROD = os.getenv("ENV") == "production"
+APP_PATH = os.getenv("APP_PATH", "/app2")
+ENV_TYPE = os.getenv("ENV", "production")
 
 
 app = FastAPI(
     title="Proxfleet API",
     description=description,
-    root_path="/app2/api", 
-    docs_url=None if IS_PROD else "/docs",
-    redoc_url=None if IS_PROD else "/redoc",
-    openapi_url=None if IS_PROD else "/openapi.json"
+    root_path=f"{APP_PATH}/api",
+    docs_url=None if ENV_TYPE == "production" else "/docs",
+    redoc_url=None if ENV_TYPE == "production" else "/redoc",
+    openapi_url=None if ENV_TYPE == "production" else "/openapi.json"
 )
 
 origins = [
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const base_path = process.env.VITE_BASE_PATH || '/app2/';
+
 export default defineConfig({
   plugins: [react()],
-  base: '/app2/',
+  base: base_path,
   server: {
     proxy: {
-      '/app2/api': {
+      [`${base_path}api`]: {
         target: 'http://localhost:8000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/app2\/api/, '')
+        rewrite: (path) => path.replace(new RegExp(`^${base_path}api`), '')
       },
     },
   },
